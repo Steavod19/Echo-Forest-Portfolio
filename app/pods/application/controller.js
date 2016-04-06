b092df1cc256a1df85b6bd56c90c12e3e485f4d1
Steve Rogers <srogers.design@gmail.com>
2016-04-06 13:29:36 -0400

    added animation to menu

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -7,15 +7,30 @@ export default Ember.Controller.extend({
   actions: {
     menuDisplay: function() {
       this.toggleProperty('isDisplayMenu');
-      // Ember.run.scheduleOnce('afterRender', this, function(){
+      Ember.run.scheduleOnce('afterRender', this, function(){
       	// perform your jQuery logic here
-        // $(".mdi").animate({  borderSpacing: -45 }, {
-        //   step: function(now,fx) {
-        //     $(this).css('-webkit-transform','rotate('+now+'deg)');
-        //     $(this).css('-moz-transform','rotate('+now+'deg)');
-        //     $(this).css('transform','rotate('+now+'deg)');
-        //   }, duration:'slow' },'linear');
-        //       });
+        if (this.get('isDisplayMenu')) {
+          $(".mdi-add-circle-outline").animate({  borderSpacing: -225 }, {
+            step: function(now,fx) {
+              $(this).css('-webkit-transform','rotate('+now+'deg)');
+              $(this).css('-moz-transform','rotate('+now+'deg)');
+              $(this).css('transform','rotate('+now+'deg)');
+            }, duration:'slow' },'linear');
+          } else {
+
+            $(".mdi-add-circle-outline").css('-webkit-transform','rotate(45deg)');
+            $(".mdi-add-circle-outline").css('-moz-transform','rotate(45deg)');
+            $(".mdi-add-circle-outline").css('transform','rotate(45deg)');
+
+            $(".mdi-add-circle-outline").animate({  borderSpacing: 270 }, {
+              step: function(now,fx) {
+                $(this).css('-webkit-transform','rotate('+now+'deg)');
+                $(this).css('-moz-transform','rotate('+now+'deg)');
+                $(this).css('transform','rotate('+now+'deg)');
+              }, duration:'slow' },'linear');
+          }
+
+            });
 
     },
   },

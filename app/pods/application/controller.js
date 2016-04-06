631f6ca12cfdc77cb92b22ef27cbfb7d6af23de4
Steve Rogers <srogers.design@gmail.com>
2016-04-06 14:03:13 -0400

    js adjustment to animation

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -5,32 +5,41 @@ export default Ember.Controller.extend({
   isDisplayMenu: false,
 
   actions: {
-    menuDisplay: function() {
+    menuDisplay() {
       this.toggleProperty('isDisplayMenu');
-      Ember.run.scheduleOnce('afterRender', this, function(){
-      	// perform your jQuery logic here
+      var self = this;
+      Ember.run.next(this, function() {
+
         if (this.get('isDisplayMenu')) {
-          $(".mdi-add-circle-outline").animate({  borderSpacing: -225 }, {
-            step: function(now,fx) {
-              $(this).css('-webkit-transform','rotate('+now+'deg)');
-              $(this).css('-moz-transform','rotate('+now+'deg)');
-              $(this).css('transform','rotate('+now+'deg)');
-            }, duration:'slow' },'linear');
-          } else {
+          $(".mdi-add-circle-outline").animate({
+            borderSpacing: -225
+          }, {
+            step: function(now) {
+              $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
+              $(this).css('-moz-transform', 'rotate(' + now + 'deg)');
+              $(this).css('transform', 'rotate(' + now + 'deg)');
+            },
+            duration: 'slow'
+          }, 'linear');
+        } else {
 
-            $(".mdi-add-circle-outline").css('-webkit-transform','rotate(45deg)');
-            $(".mdi-add-circle-outline").css('-moz-transform','rotate(45deg)');
-            $(".mdi-add-circle-outline").css('transform','rotate(45deg)');
+          $(".mdi-add-circle-outline").css('-webkit-transform', 'rotate(45deg)');
+          $(".mdi-add-circle-outline").css('-moz-transform', 'rotate(45deg)');
+          $(".mdi-add-circle-outline").css('transform', 'rotate(45deg)');
 
-            $(".mdi-add-circle-outline").animate({  borderSpacing: 270 }, {
-              step: function(now,fx) {
-                $(this).css('-webkit-transform','rotate('+now+'deg)');
-                $(this).css('-moz-transform','rotate('+now+'deg)');
-                $(this).css('transform','rotate('+now+'deg)');
-              }, duration:'slow' },'linear');
-          }
+          $(".mdi-add-circle-outline").animate({
+            borderSpacing: 270
+          }, {
+            step: function(now) {
+              $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
+              $(this).css('-moz-transform', 'rotate(' + now + 'deg)');
+              $(this).css('transform', 'rotate(' + now + 'deg)');
+            },
+            duration: 'slow'
+          }, 'linear');
+        }
 
-            });
+      });
 
     },
   },

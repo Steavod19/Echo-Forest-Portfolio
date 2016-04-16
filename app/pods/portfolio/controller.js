4b74a205ad871519f93a7485dd6dee2735d8bd7a
Steve Rogers <srogers.design@gmail.com>
2016-04-15 22:27:08 -0400

    tweaks to image layout on porfolio component

--- app/pods/portfolio/controller.js
+++ app/pods/portfolio/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   init: function() {
-    this.set('portItem', this.get('portfolioList')['1']);
+    this.set('portItem', this.get('portfolioList')['0']);
   }.on('init'),
 
   portfolioList: [
@@ -11,28 +11,28 @@ export default Ember.Controller.extend({
       value: 'Fidelity',
       key: 0,
       description: 'Currently under NDA',
-      url: 'http://placehold.it/800x400',
+      url: 'https://s3-us-west-2.amazonaws.com/port-blog/fidelity300.png',
       siteLink:'/#',
     }),
     Ember.Object.create({
       value: 'DLIFLC',
       key: 1,
       description: 'Wordpress CMS rebranded and built from the ground up',
-      url: 'https://s3-us-west-2.amazonaws.com/port-blog/dli-edu-mil.png',
+      url: 'https://s3-us-west-2.amazonaws.com/port-blog/dliflc300.png',
       siteLink:'http://dliflc.edu',
     }),
     Ember.Object.create({
       value: 'K+S',
       key: 2,
       description: 'Wedding website built with Ember.js and Bootstrap with custom side nav.',
-      url: 'https://s3-us-west-2.amazonaws.com/port-blog/K%2BS-wedding.png',
+      url: 'https://s3-us-west-2.amazonaws.com/port-blog/ks300.png',
       siteLink:'https://steveandkyrstin.com/',
     }),
     Ember.Object.create({
       value: 'PANGR',
       key: 3,
       description: 'A group blogging site built in Rails. Pangr focuses on photo blogging Pangr allows you to create groups and post photos, gifs and posts to share with group followers. It utilizes Foundation front end with a mobile ready design Carrierwave with image scaling and Amazon S3 for back end storage.',
-      url: 'https://s3-us-west-2.amazonaws.com/port-blog/pangr.png',
+      url: 'https://s3-us-west-2.amazonaws.com/port-blog/pangr300.png',
       siteLink:'http://pangr.herokuapp.com/',
     }),
   ],
